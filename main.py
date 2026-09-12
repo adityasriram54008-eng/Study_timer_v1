b0f57a9817caf256c7b153b7ec262fd0bdfdb34e
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-12 19:04:50 +0530

    implemented tabs

--- main.py
+++ main.py
@@ -8,58 +8,119 @@ st.text("\n")
 st.text("\n")
 st.text("\n")
 st.text("\n")
+tab1, tab2 = st.tabs(["STOPWATCH","POMODORO"])
 
-with st.container( horizontal_alignment="center", width = "stretch"):
+with tab1:
+    with st.container( horizontal_alignment="center", width = "stretch"):
 
-    if "running" not in st.session_state:
-        st.session_state.running = False
+        if "running" not in st.session_state:
+            st.session_state.running = False
 
-    if "start_time" not in st.session_state:
-        st.session_state.start_time = None
+        if "start_time" not in st.session_state:
+            st.session_state.start_time = None
 
-    timer_display = st.empty()
+        timer_display = st.empty()
 
-    if st.button("Start Timer"):
-        st.session_state.running = True
-        st.session_state.start_time = time.time()
+        if st.button("Start Timer"):
+            st.session_state.running = True
+            st.session_state.start_time = time.time()
 
-    if "paused" not in st.session_state:
-        st.session_state.paused = 0
+        if "paused" not in st.session_state:
+            st.session_state.paused = 0
 
-    if st.button("Pause/Resume"):
-        if st.session_state.running:
+        if st.button("Pause/Resume"):
+            if st.session_state.running:
+                st.session_state.running = False
+                stopped = int(time.time() - st.session_state.start_time)
+                st.session_state.paused = stopped
+                hours = stopped // 3600
+                minutes = (stopped % 3600) // 60
+                seconds = stopped % 60
+                timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}", text_alignment="center")
+
+            else:
+                st.session_state.running = True
+                st.session_state.start_time = (time.time() - st.session_state.paused)
+
+        if st.button("Stop Timer"):
             st.session_state.running = False
             stopped = int(time.time() - st.session_state.start_time)
-            st.session_state.paused = stopped
             hours = stopped // 3600
             minutes = (stopped % 3600) // 60
             seconds = stopped % 60
             timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}", text_alignment="center")
 
-        else:
-            st.session_state.running = True
-            st.session_state.start_time = (time.time() - st.session_state.paused)
+        if st.button("Reset Timer"):
+            st.session_state.running = False
+            st.session_state.start_time = 0
+            st.session_state.paused = 0
 
-    if st.button("Stop Timer"):
-        st.session_state.running = False
-        stopped = int(time.time() - st.session_state.start_time)
-        hours = stopped // 3600
-        minutes = (stopped % 3600) // 60
-        seconds = stopped % 60
-        timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}", text_alignment="center")
+        @st.fragment(run_every=1)
+        def timer():
+            if st.session_state.running:
+                elapsed = int(time.time() - st.session_state.start_time)
+                hours = elapsed // 3600
+                minutes = (elapsed % 3600) // 60
+                seconds = elapsed % 60
+                timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}",text_alignment="center")
 
-    if st.button("Reset Timer"):
-        st.session_state.running = False
-        st.session_state.start_time = 0
-        st.session_state.paused = 0
+        timer()
 
-    @st.fragment(run_every=1)
-    def timer():
-        if st.session_state.running:
-            elapsed = int(time.time() - st.session_state.start_time)
-            hours = elapsed // 3600
-            minutes = (elapsed % 3600) // 60
-            seconds = elapsed % 60
-            timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}",text_alignment="center")
+    with tab2:
+        duration = 25*60
+        with st.container(horizontal_alignment="center", width="stretch"):
 
-    timer()
+            if "running" not in st.session_state:
+                st.session_state.running = False
+
+            if "start_time" not in st.session_state:
+                st.session_state.start_time = None
+
+            timer_display = st.empty()
+
+            if st.button("Start Timer"):
+                st.session_state.running = True
+                st.session_state.start_time = time.time()
+
+            if "paused" not in st.session_state:
+                st.session_state.paused = 0
+
+            if st.button("Pause/Resume"):
+                if st.session_state.running:
+                    st.session_state.running = False
+                    stopped = int(time.time() - st.session_state.start_time)
+                    st.session_state.paused = stopped
+                    hours = stopped // 3600
+                    minutes = (stopped % 3600) // 60
+                    seconds = stopped % 60
+                    timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}", text_alignment="center")
+
+                else:
+                    st.session_state.running = True
+                    st.session_state.start_time = (time.time() - st.session_state.paused)
+
+            if st.button("Stop Timer"):
+                st.session_state.running = False
+                stopped = int(time.time() - st.session_state.start_time)
+                hours = stopped // 3600
+                minutes = (stopped % 3600) // 60
+                seconds = stopped % 60
+                timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}", text_alignment="center")
+
+            if st.button("Reset Timer"):
+                st.session_state.running = False
+                st.session_state.start_time = 0
+                st.session_state.paused = 0
+
+
+            @st.fragment(run_every=1)
+            def timer():
+                if st.session_state.running:
+                    elapsed = int(time.time() - st.session_state.start_time)
+                    hours = elapsed // 3600
+                    minutes = (elapsed % 3600) // 60
+                    seconds = elapsed % 60
+                    timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}", text_alignment="center")
+
+
+            timer()
