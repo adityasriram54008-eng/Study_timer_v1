ef6e2610d6e9658759bbdb142e0c09000702fcfa
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-12 18:47:17 +0530

    neat code

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ st.set_page_config(layout="wide")
 st.title(":orange[**STUDY**] TIMER", text_alignment="center")
 
 with st.container(border=True):
-
     if "running" not in st.session_state:
         st.session_state.running = False
 
     if "start_time" not in st.session_state:
         st.session_state.start_time = None
 
-    # Fixed timer location
     timer_display = st.empty()
 
     if st.button("Start Timer"):
@@ -25,32 +23,23 @@ with st.container(border=True):
     if st.button("Pause/Resume"):
         if st.session_state.running:
             st.session_state.running = False
-
             stopped = int(time.time() - st.session_state.start_time)
-
             st.session_state.paused = stopped
-
             hours = stopped // 3600
             minutes = (stopped % 3600) // 60
             seconds = stopped % 60
-
             timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
 
         else:
             st.session_state.running = True
-            st.session_state.start_time = (
-                time.time() - st.session_state.paused
-            )
+            st.session_state.start_time = (time.time() - st.session_state.paused)
 
     if st.button("Stop Timer"):
         st.session_state.running = False
-
         stopped = int(time.time() - st.session_state.start_time)
-
         hours = stopped // 3600
         minutes = (stopped % 3600) // 60
         seconds = stopped % 60
-
         timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
 
     if st.button("Reset Timer"):
@@ -59,14 +48,11 @@ with st.container(border=True):
 
     @st.fragment(run_every=1)
     def timer():
-
         if st.session_state.running:
             elapsed = int(time.time() - st.session_state.start_time)
-
             hours = elapsed // 3600
             minutes = (elapsed % 3600) // 60
             seconds = elapsed % 60
-
             timer_display.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
 
     timer()
