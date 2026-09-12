9b84253cb012f42cd8dabe2f591c3f60e03f8944
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-12 17:11:56 +0530

    implemented pause/resume

--- main.py
+++ main.py
@@ -22,22 +22,20 @@ if st.button("Stop Timer"):
     seconds = stopped % 60
     st.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
 
-if st.button("Pause"):
-    st.session_state.running = False
-    st.session_state.paused = time.time()
-    paused = int(time.time() - st.session_state.start_time)
-    hours = paused // 3600
-    minutes = (paused % 3600) // 60
-    seconds = paused % 60
-    st.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
-
-if st.button("Resume"):
-    st.session_state.running = True
-    resume = int(st.session_state.paused)
-    hours = resume // 3600
-    minutes = (resume % 3600) // 60
-    seconds = resume % 60
-    st.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
+if "paused" not in st.session_state:
+    st.session_state.paused = 0
+if st.button("Pause/Resume"):
+    if st.session_state.running:
+        st.session_state.running = False
+        stopped = int(time.time() - st.session_state.start_time)
+        st.session_state.paused = stopped
+        hours = stopped // 3600
+        minutes = (stopped % 3600) // 60
+        seconds = stopped % 60
+        st.header(f"{hours:02d}:{minutes:02d}:{seconds:02d}")
+    else:
+        st.session_state.running = True
+        st.session_state.start_time = (time.time() - st.session_state.paused)
 
 @st.fragment(run_every=1)
 def timer():
